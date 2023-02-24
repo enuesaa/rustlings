a902f3c626f110ed77d5a3fc0fec52a5d801ae40
enuesaa <nsrciog@gmail.com>
2023-02-25 00:40:11 +0900

    feat: try_from_into

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -23,8 +23,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -38,6 +36,14 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if let Some(red) = u8::try_from(tuple.0).ok() {
+            if let Some(green) = u8::try_from(tuple.1).ok() {
+                if let Some(blue) = u8::try_from(tuple.2).ok() {
+                    return Ok(Color { red, blue, green });
+                };
+            };
+        };
+        Err(IntoColorError::IntConversion)
     }
 }
 
@@ -45,6 +51,14 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if let Some(red) = u8::try_from(arr[0]).ok() {
+            if let Some(green) = u8::try_from(arr[1]).ok() {
+                if let Some(blue) = u8::try_from(arr[2]).ok() {
+                    return Ok(Color { red, blue, green });
+                };
+            };
+        };
+        Err(IntoColorError::IntConversion)
     }
 }
 
@@ -52,6 +66,17 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        };
+        if let Some(red) = u8::try_from(slice[0]).ok() {
+            if let Some(green) = u8::try_from(slice[1]).ok() {
+                if let Some(blue) = u8::try_from(slice[2]).ok() {
+                    return Ok(Color { red, blue, green });
+                };
+            };
+        };
+        Err(IntoColorError::IntConversion)
     }
 }
 
